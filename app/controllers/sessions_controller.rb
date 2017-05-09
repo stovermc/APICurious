562961504f7d40c3cb8a3408dd1975c6f3a1c225
stovermc <stovermc@gmail.com>
2017-05-08 23:27:33 -0600

    Add delete session functionality
    * added route
    
    * added destroy action to session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,12 @@ class SessionsController < ApplicationController
     if user = User.from_omniauth(request.env["omniauth.auth"])
       session[:user_id] = user.id
     end
+    redirect_to dashboard_path
+  end
+
+  def destroy
+    reset_session
+
     redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 
   root 'home#index'
 
-  get '/auth/github', as: :github_login
+  get '/auth/github',          as: :github_login
   get '/auth/github/callback', to: 'sessions#create'
 
+  delete '/logout',            to: 'sessions#destroy'
 end
