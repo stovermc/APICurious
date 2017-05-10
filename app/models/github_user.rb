d0cde7d3a4090ba91009ca1971105fab0ff517ad
stovermc <stovermc@gmail.com>
2017-05-09 22:58:21 -0600

    Add GithubUser model
    * tests passing

--- app/models/github_user.rb
+++ app/models/github_user.rb
@@ -1,4 +1,17 @@
 class GithubUser
+  attr_reader :profile_pic, :name, :starred_repos, :followers, :following
+  
+  def initialize(attrs = {})
+    @attrs          = attrs
+    @profile_pic   = attrs[:avatar_url]
+    @name          = attrs[:name]
+    @starred_repos = attrs[:starred_url]
+    @followers     = attrs[:followers_url]
+    @following     = attrs[:following_url]
+
+  end
+
+
   def self.find_user(token)
     raw_user = GithubService.user_by(token)
     new(raw_user)
--- spec/models/github_user_spec.rb
+++ spec/models/github_user_spec.rb
@@ -5,12 +5,13 @@ describe GithubUser do
     it "returns user assocaited with given token" do
       token = ENV['github_user_token']
       user = GithubUser.find_user(token)
-
-      expect(user.count).to eq(1)
+      
+      expect(user.class).to be(GithubUser)
+      expect(user.profile_pic).to eq "https://avatars0.githubusercontent.com/u/15877604?v=3"
       expect(user.name).to eq('Mark Stover')
-      expect(user.followers.count).to eq(7)
-      expect(user.following.count).to eq(12)
-      expect(user.starred_repos.count).to eq(1)
+      expect(user.starred_repos).to eq "https://api.github.com/users/stovermc/starred{/owner}{/repo}"
+      expect(user.followers).to eq "https://api.github.com/users/stovermc/followers"
+      expect(user.following).to eq "https://api.github.com/users/stovermc/following{/other_user}"
 
     end
   end
