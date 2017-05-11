0b48595628f4baae2d47812556efb5707452bca1
stovermc <stovermc@gmail.com>
2017-05-11 13:32:37 -0600

    Add following functionality
    * add route, controller and view for following index

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   get '/auth/github/callback', to: 'sessions#create'
   delete '/logout',            to: 'sessions#destroy'
 
-  # resources :dashboard, only: [:index]
   get '/dashboard',            to: 'dashboard#index'
   get '/dashboard/followers',   to: 'followers#index'
-
+  get '/dashboard/following',   to: 'following#index'
+  
 end
--- spec/features/user_can_view_followers_spec.rb
+++ spec/features/user_can_view_followers_spec.rb
@@ -14,4 +14,11 @@ describe "when a logged in user clicks on followers" do
     expect(page).to have_css("img[src='https://avatars3.githubusercontent.com/u/20036943?v=3']")
     expect(page).to have_css("img[src='https://avatars2.githubusercontent.com/u/5198260?v=3']")
   end
+
+  scenario "they see thier followers username", vcr: true do
+    click_on 'Followers'
+
+    expect(page).to have_content('josh-works')
+    expect(page).to have_content('Carmer')
+  end
 end
